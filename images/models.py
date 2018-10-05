654351fe1dd0e9b9868b9f113b4a1487e0989d39
Tom Chege <emdeechege@gmail.com>
2018-10-05 15:02:50 +0300

    can display comment by image

--- images/models.py
+++ images/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 from tinymce.models import HTMLField
+
 # Create your models here.
 class Profile(models.Model):
     profile_photo= models.ImageField(upload_to='picha/')
     bio= models.CharField(max_length=240)
-    username = models.ForeignKey(User,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+
 
     def save_profile(self):
         self.save()
@@ -15,13 +17,14 @@ class Profile(models.Model):
         profile= cls.objects.get(id=id)
         return profile
 
-    class Meta:
-        ordering = ['username']
-
+    
 class Comment(models.Model):
-    comment = models.CharField(max_length=70, blank=True)
-    username = models.ForeignKey(User,on_delete=models.CASCADE)
+    comment = models.CharField(max_length=240, blank=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    admirer= models.ForeignKey('images.Image',on_delete=models.CASCADE, related_name='image')
 
+    def save_comment(self):
+        self.save()
 
     def __str__(self):
         return self.comment
@@ -31,8 +34,9 @@ class Image(models.Model):
     insta_image = models.ImageField(upload_to='picha/')
     caption = models.CharField(max_length=70)
     like=models.IntegerField(default=0)
+    user = models.ForeignKey(User,on_delete=models.CASCADE, related_name='posts')
+    thoughts = models.ForeignKey(Comment,on_delete=models.CASCADE, null=True, blank=True)
     profile= models.ForeignKey(Profile)
-    comments = models.ForeignKey(Comment,on_delete=models.CASCADE, null=True, blank=True)
 
     @classmethod
     def all_images(self):
@@ -41,4 +45,4 @@ class Image(models.Model):
 
 class Likes(models.Model):
     likes= models.IntegerField (default=0)
-    image_id = models.ForeignKey(Image)
+    liker = models.CharField(max_length=20)
