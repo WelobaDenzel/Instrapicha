7111d5ab88ec648d4339f750e207d983eea3ab75
Tom Chege <emdeechege@gmail.com>
2018-10-06 07:50:33 +0300

    changed models again....

--- images/models.py
+++ images/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
-from tinymce.models import HTMLField
+# from tinymce.models import HTMLField
 
 # Create your models here.
 class Profile(models.Model):
-    profile_photo= models.ImageField(upload_to='picha/',null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE,null=True)
+    profile_photo= models.ImageField(upload_to='profiles/',null=True)
     bio= models.CharField(max_length=240, null=True)
-    user = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
 
 
     def save_profile(self):
@@ -21,10 +21,27 @@ class Profile(models.Model):
         return self.user
 
 
+class Image(models.Model):
+    posted_by = models.ForeignKey(User, null=True)
+    insta_image = models.ImageField(upload_to='picha/',null=True)
+    caption = models.TextField(null=True)
+
+
+
+    @classmethod
+    def all_images(self):
+        all_images = cls.objects.all()
+        return Image.objects.all()
+
+    @classmethod
+    def get_user_images(cls, profile_id):
+        images=Image.objects.filter(profile_id=user.id)
+
 class Comment(models.Model):
-    comment = models.CharField(max_length=240, blank=True)
-    user = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
-    imagecomment= models.ForeignKey('images.Image',on_delete=models.CASCADE, related_name='image',null=True)
+    poster = models.ForeignKey(User, related_name='comments',null=True)
+    image = models.ForeignKey(Image, related_name='comments',null=True)
+    comment = models.TextField(null=True)
+
 
     def save_comment(self):
         self.save()
@@ -32,24 +49,6 @@ class Comment(models.Model):
     def __str__(self):
         return self.comment
 
-
-class Image(models.Model):
-    insta_image = models.ImageField(upload_to='picha/',null=True)
-    caption = models.CharField(max_length=70,null=True)
-    like=models.IntegerField(default=0)
-    user = models.ForeignKey(User,on_delete=models.CASCADE, related_name='posts',null=True)
-    thoughts = models.ForeignKey(Comment,on_delete=models.CASCADE, null=True)
-    profile= models.ForeignKey(Profile,null=True)
-
-    @classmethod
-    def all_images(self):
-
-        return Image.objects.all()
-
-    @classmethod
-    def get_user_images(cls, profile_id):
-        images=Image.objects.filter(profile_id=user.id)
-
 class Likes(models.Model):
-    likes= models.IntegerField (default=0)
-    liker = models.CharField(max_length=20)
+    image = models.ForeignKey(Image, related_name='likes',null=True)
+    liked_by = models.ForeignKey(User, related_name='liked_images',null=True)
