774847c79330fbc9391a72420b318bde8c0b43ff
Tom Chege <emdeechege@gmail.com>
2018-10-07 16:38:19 +0300

    issues with pk after authenticating emails

--- images/urls.py
+++ images/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', views.home,name='home'),
-    url(r'^accounts/profile/(\d+)', views.profile, name='profile'),
+    url(r'^accounts/profile/', views.add_profile, name='add_profile'),
+    url(r'^profile/(\d+)', views.profile, name='profile'),
     url(r'^search/', views.search_results, name='search_results'),
     url(r'^image/(\d+)',views.get_image_by_id,name ='image'),
     url(r'^new/profile$', views.add_profile, name='add_profile'),
